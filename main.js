5b19f04a602b66d9e51a4d7c7072625cd83e26ff
Abd <abdulrahmanmaherag@gmail.com>
2024-02-28 06:13:28 +0300

    edited the title in the DOM
    added the divs in html needed to manipulate them
    in the DOM

---
+++ main.js
@@ -0,0 +1,7 @@
+const title = document.querySelector('#title');
+title.textContent =' Etch-a-Sketch';
+title.style.backgroundColor = 'blue';
+title.style.textAlign = 'center';
+title.style.padding = '12px'
+title.style.color = 'white'
+title.style.fontSize = '20px'
